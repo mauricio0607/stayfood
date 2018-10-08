b5bae5c416670ebdf3d12ad5c0908d1fe5c02c79
mauricio <mauricio_dias06@hotmail.com>
2018-10-08 18:09:12 -0300

    Initial commit

---
+++ src/configs/firebase.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyCWtfWs2m6UvuhgaznxWEH8SxNcSaq_a1I",
+    authDomain: "pizza-app-543da.firebaseapp.com",
+    databaseURL: "https://minha-entrega-3f237.firebaseio.com",
+    projectId: "minha-entrega-3f237",
+    storageBucket: "minha-entrega-3f237.appspot.com",
+    messagingSenderId: "382118263151"
+}
